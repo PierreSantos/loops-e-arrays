e08ea8af70fca40264ce0e5dce24866813bba562
Pierre <pierre.santos_2011@hotmail.com>
2023-03-15 10:21:21 -0300

    first commit

--- src/br/com/dio/exercicios/loops/Ex5_Tabuada.java
+++ src/br/com/dio/exercicios/loops/Ex5_Tabuada.java
@@ -1,7 +1,5 @@
 package br.com.dio.exercicios.loops;
 
-import java.util.Scanner;
-
 /*
 Desenvolva um gerador de tabuada,
 capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10.
@@ -15,16 +13,6 @@ Tabuada de 5:
 5 X 10 = 50
 */
 public class Ex5_Tabuada {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Tabuada: ");
-        int tabuada = scan.nextInt();
-
-        System.out.println("Tabuada de " + tabuada);
-        for(int i = 1; i <= 10; i = i + 1) {
-            System.out.println(tabuada + " X " + i + " = " + (tabuada*i));
-        }
-    }
+    
 
 }
